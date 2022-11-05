dc2c1c50f4f5630199338d12c92c0a51d2b5cdfa
Shital769 <110831382+Shital769@users.noreply.github.com>
2022-11-06 00:10:20 +1100

    extracted a reusable Http service

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
-
-axios.interceptors.response.use(null, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    alert("An unexpected error occured.");
-  }
-});
 
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 class App extends Component {
@@ -21,13 +9,13 @@ class App extends Component {
   };
   async componentDidMount() {
     //promise method will do promise > resolved (succes) OR rejected (failure)
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -35,7 +23,7 @@ class App extends Component {
 
   handleUpdate = async (post) => {
     post.title = "UPDATED";
-    await axios.put(apiEndpoint + "/" + post.id, post);
+    await http.put(apiEndpoint + "/" + post.id, post);
     //put method to update all properties
     // patch method to update one or more properties
     const posts = [...this.state.posts];
@@ -51,7 +39,7 @@ class App extends Component {
     this.setState({ posts });
     try {
       //for expected error
-      await axios.delete(apiEndpoint + "/999" + post.id);
+      await http.delete(apiEndpoint + "/999" + post.id);
       //for unexpected error
       //await axios.delete("s" + apiEndpoint + "/" + post.id);
       throw new Error("");
